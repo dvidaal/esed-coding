c62e27ad58333f81bb2927b0b1a19e0f2d516e95
Diana Vidal <dvidaal@gmail.com>
2024-01-16 21:58:20 +0100

    Fix error with header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Form from "./components/Form/Form";
-import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
       <main className="main-container">
         <Form />
       </main>
