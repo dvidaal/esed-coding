42005333c46a7eec9844d0ca98b1d371977a4e7e
Diana Vidal <dvidaal@gmail.com>
2024-01-17 18:18:17 +0100

    Create a test for the button

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Given a Button component", () => {
+  describe("When rendered", () => {
+    test("Then it should show a button with the text 'Next' on it", () => {
+      const buttonText = "Next";
+      render(<Button isDisabled={true} text={buttonText} />);
+
+      const expectedButton = screen.getByRole("button");
+
+      expect(expectedButton).toBeInTheDocument();
+    });
+  });
+});
